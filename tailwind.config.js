16a4fc72af5a8b92c5c8c2062dfabd7292ba38dc
Kevin Marsh <kevin.marsh@gmail.com>
2020-12-15 13:59:17 -0500

    Gets Tailwind working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 module.exports = {
   purge: [
     './layouts/**/*.html',
+    './content/**/*.md',
+    './content/**/*.html',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          light: '#e9f2f9',
+          DEFAULT: '#0b89cc',
+        },
+      },
+      fontFamily: {
+        noto: ['Noto Serif', 'serif'],
+      },
+    },
   },
   variants: {
     extend: {},
